ba9b0e7bf732e629c989573d3eb440dd49534488
Paul Felton <paul.r.felton@gmail.com>
2020-03-27 09:37:21 -0500

    Challenge 3 code to use 2 loops to print board

--- src/Board.js
+++ src/Board.js
@@ -11,26 +11,48 @@ class Board extends React.Component {
       );
     }
 
-    /*
+    /*Challenge 3 code
     Figure out a way to create a div board row every row and
     call a renderSquare() 3 times per row then move to the next row
+    create 2 arrays one is boardTable with 3 arrays of 3 boardRow that has 3 rendered squares
     */
     printBoard (){
-      let row = 0;
-      let col = 0;
-    
-      for(;row < 3;row++){
-        let buttonRow = []
-        for(;col < 3;col++){
-            buttonRow.push(renderSquare(col))
+      //Tic tac toe is always 3by3, but can add a max size incase implement connect 4 or something
+      //const boardSize = 3;
+      let boardTable = [];
+      //let buttonRow = []; prints 3 boards
+      for(let row = 0;row < 3;row++){
+        let boardRow = []; 
+        for(let col = 0;col < 3;col++){
+          //alert(row);
+          //alert(col);
+          boardRow.push(this.renderSquare((row * 3 + col)));
         }
+      boardTable.push(<div key = {row} className="board-row">{boardRow}</div>);
       }
+      //alert(row);
+
+      /* All are undefined
+      alert(boardTable[0][0]);
+      alert(boardTable[0][1]);
+      alert(boardTable[0][2]);
+      alert(boardTable[1][0]);
+      alert(boardTable[1][1]);
+      alert(boardTable[1][2]);
+      alert(boardTable[2][0]);
+      alert(boardTable[2][1]);
+      alert(boardTable[2][2]);
+      */
+
+     return boardTable;
+    
     }
 
     render() {
       return (
-
-        this.printBoard()
+        <div>
+          {this.printBoard()}
+        </div>
         
         /*
         <div>
